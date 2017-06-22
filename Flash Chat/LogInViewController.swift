8e9b4022f4867c9904a03191f254635e5536d911
Sujanth <sujanth1987@gmail.com>
2017-06-22 23:00:32 +1000

    Completed UI changes

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 
 class LogInViewController: UIViewController {
@@ -26,6 +27,8 @@ class LogInViewController: UIViewController {
 
    
     @IBAction func logInPressed(_ sender: AnyObject) {
+        
+        SVProgressHUD.show()
 
         
         //TODO: Log in the user
@@ -34,6 +37,7 @@ class LogInViewController: UIViewController {
                 print(error!)
             } else {
                 print("WOW! You made it well!")
+                SVProgressHUD.dismiss()
                 
                 self.performSegue(withIdentifier: "goToChat", sender: self)
                 
--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 
 class RegisterViewController: UIViewController {
@@ -30,6 +31,8 @@ class RegisterViewController: UIViewController {
   
     @IBAction func registerPressed(_ sender: AnyObject) {
         
+        SVProgressHUD.show()
+        
 
         
         //TODO: Set up a new user on our Firbase database
@@ -39,6 +42,8 @@ class RegisterViewController: UIViewController {
                 print(error!)
             } else {
                 print("Registration Successful!")
+                
+                SVProgressHUD.dismiss()
                 
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
