a4ed93f9020e0b5a8ee373fbf10e7dff3b6e01a9
Sujanth <sujanth1987@gmail.com>
2017-06-22 22:46:30 +1000

    Deleted initial setup code

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -21,9 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //TODO: Initialise and Configure your Firebase here:
         FIRApp.configure()
         
-        let myDatabase = FIRDatabase.database().reference()
-        myDatabase.setValue("We've got data!")
-        
         
         return true
     }
